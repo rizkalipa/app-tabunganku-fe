6a490bd0c2bc3e03ebc4363d345ac856d5d0cc97
Rizky Aji Rahmansyah <rizkyajirahmansyah@gmail.com>
2024-11-01 15:04:53 +0700

    config pwa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{vue,js}'],
   content: [],
   theme: {
     extend: {},
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/app-tabunganku-fe/',
+  pwa: {
+    name: "TabunganKu",
+    short_name: "TabunganKu",
+    display: "standalone",
+    themeColor: '#FFF',
+    msTileColor: '#FFF',
+    appleMobileWebAppStatusBarStyle: 'default',
+    description: "TabunganKu App"
+  }
 })
